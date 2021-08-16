29327d598a3aff399f40e2b959accb5d45e75542
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-08-16 17:33:14 +0300

    Extraxt class Pixel.

---
+++ Data/Pixel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public class Pixel
+    {
+        public Pixel(double r, double g, double b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public double R { get; }
+        public double G { get; }
+        public double B { get; }
+    }
+}
