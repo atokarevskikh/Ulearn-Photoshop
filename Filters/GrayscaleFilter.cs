fff7aedb8bbc7fac08d1db8617f411a1ffa73137
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-08-18 18:03:40 +0300

    Add interface  IParameters and abstract class ParametrizedFilter.

--- Filters/GrayscaleFilter.cs
+++ Filters/GrayscaleFilter.cs
@@ -4,17 +4,14 @@ namespace MyPhotoshop
 {
     public class GrayscaleFilter : PixelFilter
     {
-        public override  ParameterInfo[] GetParameters()
-        {
-            return new ParameterInfo[0];
-        }
+        public GrayscaleFilter() : base(new EmptyParameters()) {}
 
         public override string ToString()
         {
             return "Оттенки серого";
         }
 
-        public override Pixel ProcessPixel(Pixel original, double[] parameters)
+        public override Pixel ProcessPixel(Pixel original, IParameters parameters)
         {
             var lightness = 0.2126 * original.R + 0.7152 * original.G + 0.0722 * original.B;
             return new Pixel(lightness, lightness, lightness);
--- Filters/PixelFilter.cs
+++ Filters/PixelFilter.cs
@@ -5,12 +5,13 @@ using System.Text;
 
 namespace MyPhotoshop
 {
-    public abstract class PixelFilter : IFilter
+    public abstract class PixelFilter : ParametrizedFilter
     {
-        public abstract ParameterInfo[] GetParameters();
-        public abstract Pixel ProcessPixel(Pixel original, double[] parameters);
+        protected PixelFilter(IParameters parameters) : base(parameters) {}
 
-        public Photo Process(Photo original, double[] parameters)
+        public abstract Pixel ProcessPixel(Pixel original, IParameters parameters);
+
+        public override Photo Process(Photo original, IParameters parameters)
         {
             var result = new Photo(original.width, original.height);
             for (int x = 0; x < result.width; x++)
