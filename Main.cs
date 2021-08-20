bfdfbbfe148cc8639644058dfd766010a18e224c
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-08-20 17:04:06 +0300

    Add class TransformFilter and two its examples.

--- Main.cs
+++ Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MyPhotoshop
@@ -19,6 +20,16 @@ namespace MyPhotoshop
 					var lightness = 0.2126 * original.R + 0.7152 * original.G + 0.0722 * original.B;
 					return new Pixel(lightness, lightness, lightness);
 				}
+			));
+			window.AddFilter(new TransformFilter(
+				"Повернуть на -90 градусов",
+				size => new Size(size.Height, size.Width),
+				(point, size) => new Point(size.Width - point.Y - 1, point.X)
+			));
+			window.AddFilter(new TransformFilter(
+				"Отразить по горизонтали",
+				size => size,
+				(point, size) => new Point(size.Width - point.X - 1, point.Y)
 			));
 			Application.Run (window);
 		}
